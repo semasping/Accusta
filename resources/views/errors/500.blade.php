151f7f5dad286f4079a5c345f9e5b3eabc4761b6
semasping <semasping@gmail.com>
2018-07-01 13:32:44 +0200

    [update] error logging with sentry

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -5,13 +5,13 @@
         <div class="subtitle">Error ID: {{ Sentry::getLastEventID() }}</div>
 
         <!-- Sentry JS SDK 2.1.+ required -->
-        <script src="https://cdn.ravenjs.com/3.3.0/raven.min.js"></script>
+        <script src="https://cdn.ravenjs.com/3.23.1/raven.min.js"></script>
 
         <script>
             Raven.showReportDialog({
                 eventId: '{{ Sentry::getLastEventID() }}',
                 // use the public DSN (dont include your secret!)
-                dsn: 'https://e9ebbd88548a441288393c457ec90441@sentry.io/3235',
+                dsn: 'https://cffcbe4280f44df2abdb1343db8fbebd@sentry.io/291781',
                 user: {
                     'name': 'Jane Doe',
                     'email': 'jane.doe@example.com',
