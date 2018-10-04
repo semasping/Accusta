df10d7d5a6b574ca5fc613461f45f565874f933d
SemasPing <semasping@gmail.com>
2018-10-04 23:51:47 +0200

    [update] footer text

--- resources/views/steemit/footer.blade.php
+++ resources/views/steemit/footer.blade.php
@@ -3,12 +3,9 @@
         <div class="row links ">
             <center>
                 <p class="text-muted links">Developing by:<a href="https://steemit.com/@semasping">@semasping</a>. </p>
-                <p> Support the developers <a href="https://steemit.com/@ukrainian.trail">@Ukrainian.trail</a> by
-                    voting for our witness <a href="https://steemit.com/~witnesses">https://steemit.com/~witnesses</a> or throw <a href="https://steemconnect.com/sign/account-witness-vote?witness=ukrainian.trail&approve=1">SteemConnect</a></p>
 
                 <p> Do you want to have a daily passive income from your SteemPower? <br>Delegate your SP in our Bid-Bot <a href="https://steemit.com/@authors.league">@authors.league</a> and
-                    receive daily payments to your account in the amount of at least 90% of the earned on your delegation
-                </p>
+                    receive daily payments to your account</p>
             </center>
         </div>
     </div>
