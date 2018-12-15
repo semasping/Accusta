d9d53c57657b784299b36fd583621fc3ed9383df
SemasPing <semasping@gmail.com>
2018-12-15 22:57:04 +0200

    [all] add new lang files for use in charts

---
+++ resources/lang/en/steemit.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: semasping (semasping@gmail.com)
+ * Date: 15.12.2018
+ * Time: 14:12
+ */
+
+return [
+    'shares' => 'Steem Power',
+    'token' => 'STEEM',
+    'peg' => 'SBD',
+    'count_rewards' => 'Count rewards',
+    'title_curator' => 'Curator Rewards statistics' ,
+    'title_benefactor_in' => 'Benefactor Steem Power Rewards statistics',
+    'title_benefactor_out' => 'Benefactor Rewards from your post to others accounts',
+    'title_author_rewards_shares' => 'Author Steem Power Rewards statistics',
+    'title_author_rewards_peg' => 'Author SBD Rewards statistics',
+    'title_author_rewards_token' => 'Author STEEM Rewards statistics',
+    'title_witness_rewards_shares' => 'Witness Steem Power Rewards statistics',
+];
